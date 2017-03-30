f8acc120d34237f81b405afb931f7508f238b264
vagrant <vagrant@homestead>
2017-03-30 20:40:56 +0000

    clear junk

--- elementlist.js
+++
@@ -1,3 +0,0 @@
-
-var ApiGen = ApiGen || {};
-ApiGen.elements = [["c","AesCtrTest"],["c","AesTest"],["c","BlockCiphersGenericTest"],["c","Dcrypt\\Aes"],["c","Dcrypt\\AesCtr"],["c","Dcrypt\\Cryptobase"],["c","Dcrypt\\Hash"],["c","Dcrypt\\Mcrypt"],["c","Dcrypt\\Otp"],["c","Dcrypt\\Pkcs7"],["c","Dcrypt\\Random"],["c","Dcrypt\\Rc4"],["c","Dcrypt\\Spritz"],["c","Dcrypt\\Str"],["c","Dcrypt\\Support"],["c","HashTest"],["c","McryptTest"],["c","OtpTest"],["c","Pkcs7Test"],["c","RandomTest"],["c","Rc4Test"],["c","SpritzTest"],["c","StrcmpTest"],["c","SwapTest"],["c","TestSupport"]];
